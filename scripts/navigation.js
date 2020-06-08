d795714d678c0a3e485a5978041b49f5110d206e
bhargavkuchipudi0 <bhargavkuchipudi0@mail.com>
2020-06-08 05:06:28 -0400

    nav-bar tab change on scroll

--- scripts/navigation.js
+++ scripts/navigation.js
@@ -13,9 +13,38 @@ window.addEventListener('load', function() {
 
 window.addEventListener('scroll', function() {
     var navBar = document.getElementsByClassName('nav-bar');
-        if (window.pageYOffset >= 102) {
-            navBar[0].classList.add('fixed-nav');
-        } else {
-            navBar[0].classList.remove('fixed-nav');
-        }
+    if (window.pageYOffset >= 102) {
+        navBar[0].classList.add('fixed-nav');
+    } else {
+        navBar[0].classList.remove('fixed-nav');
+    }
+    changeNavFocus();
 });
+
+function changeNavFocus() {
+    var sections = document.getElementsByTagName('section');
+    var navs = document.getElementsByClassName('nav-link');
+    for (let i = 0; i < sections.length; i++) {
+        navs[i].classList.remove('nav-active');
+        if(isSectionInView(sections[i])) {
+            navs[i].classList.add('nav-active');
+        }
+    }
+}
+
+function isSectionInView(el) {
+    const scroll = window.scrollY || window.pageYOffset
+	const boundsTop = el.getBoundingClientRect().top + scroll
+	
+	const viewport = {
+		top: scroll,
+		bottom: scroll + window.innerHeight,
+	}
+	
+    const bounds = {
+		top: boundsTop,
+		bottom: boundsTop + el.clientHeight,
+	}
+	
+	return (bounds.top <= (viewport.top+viewport.bottom)/2) && (bounds.bottom >= ((viewport.top+viewport.bottom)/2));
+}
